1b5dad6c3e656c3e1f372b323aa4c9a2c204efba
Jkaiser1 <Jkaiser30@gmail.com>
2024-10-18 17:24:32 -0500

    cuarto commit

--- src/main/java/com/unu/poo2/beans/autor.java
+++ src/main/java/com/unu/poo2/beans/autor.java
@@ -1,5 +1,43 @@
 package com.unu.poo2.beans;
 
 public class autor {
+	private int idAutor;
+	private String nacionalidad;
+	private String NombreAutor;
+
+	public autor() {
+		super();
+	}
+
+	public autor(int idAutor, String nacionalidad, String nombreAutor) {
+		super();
+		this.idAutor = idAutor;
+		this.nacionalidad = nacionalidad;
+		NombreAutor = nombreAutor;
+	}
+
+	public int getIdAutor() {
+		return idAutor;
+	}
+
+	public void setIdAutor(int idAutor) {
+		this.idAutor = idAutor;
+	}
+
+	public String getNacionalidad() {
+		return nacionalidad;
+	}
+
+	public void setNacionalidad(String nacionalidad) {
+		this.nacionalidad = nacionalidad;
+	}
+
+	public String getNombreAutor() {
+		return NombreAutor;
+	}
+
+	public void setNombreAutor(String nombreAutor) {
+		NombreAutor = nombreAutor;
+	}
 
 }
